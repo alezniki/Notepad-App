dd29fe2db149da42612599bc231eced352e5b785
alezniki <alezniki@gmail.com>
2017-06-25 21:57:58 +0200

    Added NotesActivity class

--- app/src/main/java/com/alezniki/notepad/activities/MainActivity.java
+++ app/src/main/java/com/alezniki/notepad/activities/MainActivity.java
@@ -1,13 +1,15 @@
 package com.alezniki.notepad.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alezniki.notepad.R;
 import com.alezniki.notepad.model.DatabaseHelper;
@@ -16,6 +18,11 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseHelper helper = null;
+    public final int REQUEST_DATA_FROM_NOTES_ACTIVITY = 1;
+
+
+    private TextView tvTitle;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) setSupportActionBar(toolbar);
 
+        tvTitle = (TextView) findViewById(R.id.tv_note_title);
+        tvText = (TextView) findViewById(R.id.tv_note_text);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this,NotesActivity.class));
             }
         });
     }
@@ -54,6 +63,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_DATA_FROM_NOTES_ACTIVITY) {
+
+            if (resultCode == Activity.RESULT_CANCELED){
+                // If there is no input result from the user
+                tvTitle.getText();
+                tvText.getText();
+
+            } else if (resultCode == Activity.RESULT_OK){
+                // If user adds new note
+                String title = data.getStringExtra("note_title");
+                String text = data.getStringExtra("note_text");
+
+            }
+        }
+    }
 
     public DatabaseHelper getDatabaseHelper() {
         if (helper == null) {
---
+++ app/src/main/java/com/alezniki/notepad/activities/NotesActivity.java
@@ -0,0 +1,74 @@
+package com.alezniki.notepad.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.alezniki.notepad.R;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private EditText etTitle;
+    private EditText etText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) setSupportActionBar(toolbar);
+
+        etTitle = (EditText) findViewById(R.id.et_save_note_title);
+        etText = (EditText) findViewById(R.id.et_save_note_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_notes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_save_note) {
+            saveNote();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Send data about created note back to main activity
+    private void saveNote() {
+        String title = etTitle.getText().toString();
+        String text = etText.getText().toString();
+
+        if (title.trim().isEmpty() || text.trim().isEmpty()) {
+            // If text fields are empty return to main activity
+            Intent intent = new Intent();
+            setResult(Activity.RESULT_CANCELED, intent);
+
+            finish();
+        } else {
+            // Send note data
+            Intent intent = new Intent();
+
+            intent.putExtra("note_title",title);
+            intent.putExtra("note_text",text);
+
+            setResult(Activity.RESULT_OK,intent);
+            finish();
+        }
+    }
+
+}
